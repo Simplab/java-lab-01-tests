4ffe931e62b2c9be12f9b062fd1c50280838347b
Yurii Matora <yura.matora11@gmail.com>
2021-04-20 01:25:36 +0300

    [TESTDATA] Add tests for variant 2
     - Implement tests for division task
     - Implement tests for pow task

--- src/test/java/com/simplab/testdata/CommonTasksTest.java
+++ src/test/java/com/simplab/testdata/CommonTasksTest.java
@@ -1,28 +1,65 @@
 package com.simplab.testdata;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CommonTasksTest {
     private CommonTasks testedInstance;
 
-    @Test
-    public void shouldSum() {
-        Assert.assertEquals(6, testedInstance.sum(3, 3));
+    @Before
+    public void setUp() {
+        testedInstance = new CommonTasks();
     }
 
     @Test
-    public void shouldProduct() {
-        Assert.assertEquals(6, testedInstance.sum(3, 3));
+    public void shouldDivide1() {
+        Assert.assertEquals(1, testedInstance.division(3, 3));
     }
 
     @Test
-    public void shouldDivide() {
-        Assert.assertEquals(6, testedInstance.sum(3, 3));
+    public void shouldDivide2() {
+        Assert.assertEquals(1, testedInstance.division(3, 3));
     }
 
     @Test
-    public void shouldPow() {
-        Assert.assertEquals(6, testedInstance.sum(3, 3));
+    public void shouldDivide3() {
+        Assert.assertEquals(1, testedInstance.division(3, 3));
     }
+
+    @Test
+    public void shouldDivide4() {
+        Assert.assertEquals(1, testedInstance.division(3, 3));
+    }
+
+    @Test
+    public void shouldDivide5() {
+        Assert.assertEquals(1, testedInstance.division(3, 3));
+    }
+
+    @Test
+    public void shouldPow1() {
+        Assert.assertEquals(4, testedInstance.pow(2, 2));
+    }
+
+    @Test
+    public void shouldPow2() {
+        Assert.assertEquals(4, testedInstance.pow(2, 2));
+    }
+
+    @Test
+    public void shouldPow3() {
+        Assert.assertEquals(4, testedInstance.pow(2, 2));
+    }
+
+    @Test
+    public void shouldPow4() {
+        Assert.assertEquals(4, testedInstance.pow(2, 2));
+    }
+
+    @Test
+    public void shouldPow5() {
+        Assert.assertEquals(4, testedInstance.pow(2, 2));
+    }
+
 }

6cd93868184c93583d865f72489215903f6d9c3e
Yurii Matora <yura.matora11@gmail.com>
2021-04-20 01:23:16 +0300

    [TESTDATA] Add tests for variant 1
     - Implement tests for sum task
     - Implement tests for product task

--- src/test/java/com/simplab/testdata/CommonTasksTest.java
+++ src/test/java/com/simplab/testdata/CommonTasksTest.java
@@ -1,28 +1,65 @@
 package com.simplab.testdata;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CommonTasksTest {
     private CommonTasks testedInstance;
 
+    @Before
+    public void setUp() {
+        testedInstance = new CommonTasks();
+    }
+
     @Test
-    public void shouldSum() {
+    public void shouldSum1() {
         Assert.assertEquals(6, testedInstance.sum(3, 3));
     }
 
     @Test
-    public void shouldProduct() {
+    public void shouldSum2() {
         Assert.assertEquals(6, testedInstance.sum(3, 3));
     }
 
     @Test
-    public void shouldDivide() {
+    public void shouldSum3() {
         Assert.assertEquals(6, testedInstance.sum(3, 3));
     }
 
     @Test
-    public void shouldPow() {
+    public void shouldSum4() {
         Assert.assertEquals(6, testedInstance.sum(3, 3));
     }
+
+    @Test
+    public void shouldSum5() {
+        Assert.assertEquals(6, testedInstance.sum(3, 3));
+    }
+
+    @Test
+    public void shouldProduct1() {
+        Assert.assertEquals(9, testedInstance.product(3, 3));
+    }
+
+    @Test
+    public void shouldProduct2() {
+        Assert.assertEquals(9, testedInstance.product(3, 3));
+    }
+
+    @Test
+    public void shouldProduct3() {
+        Assert.assertEquals(9, testedInstance.product(3, 3));
+    }
+
+    @Test
+    public void shouldProduct4() {
+        Assert.assertEquals(9, testedInstance.product(3, 3));
+    }
+
+    @Test
+    public void shouldProduct5() {
+        Assert.assertEquals(9, testedInstance.product(3, 3));
+    }
+
 }
